a0d5401098e73ce41d07a5d2b36d13f16b23b8d2
amezgin <nolich@gmail.com>
2020-03-04 14:56:57 +0300

    Task 1, 3 (DTO User, Factory, parseFullName) completed

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,14 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?) : Pair<String?, String?> {
+        val parts: List<String>? = fullName?.trim()?.replace("\\s+".toRegex(), " ")?.split(" ")
+        val result = when(parts?.size) {
+            null, 0 -> null to null
+            1 -> (if (parts.get(0).isEmpty()) null else parts.get(0)) to null
+            2 -> parts.get(0) to parts.get(1)
+            else -> (if (parts.get(0).isEmpty()) null else parts.get(0)) to (if (parts.get(1).isEmpty()) null else parts.get(1))
+        }
+        return result
+    }
+}
