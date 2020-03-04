9a172b1dcca381607ed8efd278045e915452d65e
amezgin <nolich@gmail.com>
2020-03-04 20:03:28 +0300

    Task 4 (Date.format) completed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -1,6 +1,12 @@
 package ru.skillbranch.devintensive.extensions
 
+import java.text.SimpleDateFormat
 import java.util.*
+
+fun Date.format(pattern: String = "HH:mm:ss dd.MM.yy"): String {
+    val dateFormat = SimpleDateFormat(pattern, Locale("ru"))
+    return dateFormat.format(this)
+}
 
 fun Date.humanizeDiff(date: Date = Date()): String {
     //TODO
