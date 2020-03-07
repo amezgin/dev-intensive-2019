dc40ce8b32e2abb80c029da2b10a15f035f469f2
amezgin <nolich@gmail.com>
2020-03-07 23:13:10 +0300

    Task 8 (*Date.humanizeDiff) completed

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Date.kt
@@ -28,8 +28,68 @@ fun Date.add(value: Int, units: TimeUnits = TimeUnits.SECOND): Date {
 }
 
 fun Date.humanizeDiff(date: Date = Date()): String {
-    //TODO
-    return ""
+    val isFuture: Boolean
+
+    // Получаем разницу в мс
+    var diff = date.time - this.time
+    isFuture = diff <= 0
+    diff = Math.abs(diff)
+
+    if (isFuture) { // будущее
+        return when(diff) {
+            (0 * SECOND), (1 * SECOND) -> "через секунду"
+            in (1 * SECOND)..(45 * SECOND) -> "через несколько секунд"
+            in (45 * SECOND)..(75 * SECOND) -> "через минуту"
+            in (75 * SECOND)..(45 * MINUTE) -> "через ${humanizePlurals(diff / MINUTE, TimeUnits.MINUTE)}"
+            in (45 * MINUTE)..(75 * MINUTE) -> "через час"
+            in (75 * MINUTE)..(22 * HOUR) -> "через ${humanizePlurals(diff / HOUR, TimeUnits.HOUR)}"
+            in (22 * HOUR)..(26 * HOUR) -> "через день"
+            in (26 * HOUR)..(360 * DAY) -> "через ${humanizePlurals(diff / DAY, TimeUnits.DAY)}"
+            else -> "более чем через год"
+        }
+
+    } else { // прошлое
+        return when(diff) {
+            (0 * SECOND), (1 * SECOND) -> "только что"
+            in (1 * SECOND)..(45 * SECOND) -> "несколько секунд назад"
+            in (45 * SECOND)..(75 * SECOND) -> "минуту назад"
+            in (75 * SECOND)..(45 * MINUTE) -> "${humanizePlurals(diff / MINUTE, TimeUnits.MINUTE)} назад"
+            in (45 * MINUTE)..(75 * MINUTE) -> "час назад"
+            in (75 * MINUTE)..(22 * HOUR) -> "${humanizePlurals(diff / HOUR, TimeUnits.HOUR)} назад"
+            in (22 * HOUR)..(26 * HOUR) -> "день назад"
+            in (26 * HOUR)..(360 * DAY) -> "${humanizePlurals(diff / DAY, TimeUnits.DAY)} назад"
+            else -> "более года назад"
+        }
+    }
+}
+
+private fun humanizePlurals(count: Long, unit: TimeUnits): String {
+    val lastDigit: String = if (count.toString().length > 1) {
+        count.toString()[count.toString().length - 2].toString()
+    } else {
+        count.toString()
+    }
+
+    return when(lastDigit) {
+        "1", "01" -> {
+            if (TimeUnits.SECOND.equals(unit)) ""
+            else if (TimeUnits.MINUTE.equals(unit)) "$count минуту"
+            else if (TimeUnits.HOUR.equals(unit)) "$count час"
+            else "$count день"
+        }
+        "2", "02", "3", "03", "4", "04"-> {
+            if (TimeUnits.SECOND.equals(unit)) ""
+            else if (TimeUnits.MINUTE.equals(unit)) "$count минуты"
+            else if (TimeUnits.HOUR.equals(unit)) "$count часа"
+            else "$count дня"
+        }
+        else -> {
+            if (TimeUnits.SECOND.equals(unit)) ""
+            else if (TimeUnits.MINUTE.equals(unit)) "$count минут"
+            else if (TimeUnits.HOUR.equals(unit)) "$count часов"
+            else "$count дней"
+        }
+    }
 }
 
 enum class TimeUnits {
