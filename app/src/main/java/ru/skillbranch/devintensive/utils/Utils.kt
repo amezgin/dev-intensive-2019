0905ec1c21c22333ca8c5854140c73e6104d4edb
amezgin <nolich@gmail.com>
2020-03-07 22:30:55 +0300

    Task 7 (*transliteration) completed

--- app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -1,5 +1,7 @@
 package ru.skillbranch.devintensive.utils
 
+import android.annotation.SuppressLint
+
 object Utils {
     fun parseFullName(fullName: String?) : Pair<String?, String?> {
         val parts: List<String>? = fullName?.trim()?.replace("\\s+".toRegex(), " ")?.split(" ")
@@ -21,5 +23,52 @@ object Utils {
                     && !lastName?.trim()?.replace("\\s+".toRegex(), " ").isNullOrEmpty() -> lastName?.substring(0, 1)?.toUpperCase()
             else -> firstName?.trim()?.replace("\\s+".toRegex(), " ")?.substring(0, 1)?.toUpperCase() + lastName?.trim()?.replace("\\s+".toRegex(), " ")?.substring(0, 1)?.toUpperCase()
         }
+    }
+
+    @SuppressLint("DefaultLocale")
+    fun transliteration(payload: String, divider: String = " "): String? {
+        val chars = mapOf(
+            "а" to "a",
+            "б" to "b",
+            "в" to "v",
+            "г" to "g",
+            "д" to "d",
+            "е" to "e",
+            "ё" to "e",
+            "ж" to "zh",
+            "з" to "z",
+            "и" to "i",
+            "й" to "i",
+            "к" to "k",
+            "л" to "l",
+            "м" to "m",
+            "н" to "n",
+            "о" to "o",
+            "п" to "p",
+            "р" to "r",
+            "с" to "s",
+            "т" to "t",
+            "у" to "u",
+            "ф" to "f",
+            "х" to "h",
+            "ц" to "c",
+            "ч" to "ch",
+            "ш" to "sh",
+            "щ" to "sh'",
+            "ъ" to "",
+            "ы" to "i",
+            "ь" to "",
+            "э" to "e",
+            "ю" to "yu",
+            "я" to "ya",
+            " " to divider)
+
+        var result = ""
+        payload.replace(" ", divider)
+            .forEach {
+                val symbol = if(!chars[(it.toString().toLowerCase())].isNullOrEmpty()) chars[(it.toString().toLowerCase())] else it.toString()
+                result += if (it.isUpperCase()) symbol?.capitalize() else symbol
+            }
+        return result
     }
 }
