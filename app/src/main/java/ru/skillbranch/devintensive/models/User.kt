9a20731f21ed137623335b1195b197f165c4c5ed
amezgin <nolich@gmail.com>
2020-03-08 10:22:46 +0300

    Task 9 (**Паттерн Builder) completed

--- app/src/main/java/ru/skillbranch/devintensive/models/User.kt
+++ app/src/main/java/ru/skillbranch/devintensive/models/User.kt
@@ -28,4 +28,26 @@ data class User(
             return User(id = "$lastId", firstName = firstName, lastName = lastName)
         }
     }
+
+    data class Builder(
+        var id: String? = null,
+        var firstName: String? = null,
+        var lastName: String? = null,
+        var avatar: String? = null,
+        var rating: Int? = null,
+        var respect: Int? = null,
+        var lastVisit: Date? = null,
+        var isOnline: Boolean = false) {
+
+        fun id(id: String): Builder = apply { this.id = id }
+        fun firstName(firstName: String) = apply { this.firstName = firstName }
+        fun lastName(lastName: String) = apply { this.lastName = lastName }
+        fun avatar(avatar: String) = apply { this.avatar = avatar }
+        fun rating(rating: Int) = apply { this.rating = rating }
+        fun respect(respect: Int) = apply { this.respect = respect }
+        fun lastVisit(lastVisit: Date) = apply { this.lastVisit = lastVisit }
+        fun isOnline(isOnline: Boolean) = apply { this.isOnline = isOnline }
+
+        fun build() = id?.let { rating?.let { it1 -> respect?.let { it2 -> User(it, firstName, lastName, avatar, it1, it2, lastVisit, isOnline) } } }
+    }
 }
